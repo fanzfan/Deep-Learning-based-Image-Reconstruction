96e81e7ffe6cc58e4cba8d72c381284996c38661
Vela-Yang <71018134+Vela-Yang@users.noreply.github.com>
2021-07-29 22:14:38 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from torchvision.transforms import CenterCrop
 # 目前已知的，读取有问题的样本
 error_filename = [9659, 10549, 10552]
 # 让输入图片等维
-transform = CenterCrop(size=(360, 480))
+transform = CenterCrop(size=(48, 48))
 
 class VelaDataset(Dataset):
     def __init__(self,
