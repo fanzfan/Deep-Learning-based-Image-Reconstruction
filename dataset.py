1c8a30d9312c6c77cddfc9421eee018a3d047cfd
Vela-Yang <71018134+Vela-Yang@users.noreply.github.com>
2021-07-29 15:41:57 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -42,7 +42,7 @@ class VelaDataset(Dataset):
         self._walker = [i for i in self._walker if i not in error_filename]
 
     # 实现getitem方法
-    def __getitem__(self, n: int) -> tuple[Tensor, Tensor]:
+    def __getitem__(self, n: int):
         if n in error_filename:
             n = 1
         filename = self._walker[n]
